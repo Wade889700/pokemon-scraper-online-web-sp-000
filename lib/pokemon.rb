ef54005e441bc0a2400115b9385ca13f77861352
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-17 00:58:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
   end
 
   def alter_hp(new_hp)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, id)
+    db.execute("UPDATE pokemon SET hp = #{new_hp} WHERE id = #{self.id}")
   end
 
 
