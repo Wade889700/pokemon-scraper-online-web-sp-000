e0fcc7f7fc9d8d7079960d9541637cac5d4824f8
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-17 00:50:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon WHERE id = ?", #{id).flatten
+    db.execute("SELECT * FROM pokemon WHERE id = #{id}").flatten
   end
 
   def alter_hp(new_hp)
